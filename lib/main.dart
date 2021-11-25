1dd61091740bad45d172a3600f6b09f08cf9720c
Nilesh Kachare <nileshkachare728@gmail.com>
2021-11-25 17:19:57 +0530

    my first comit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key ?key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Container(child: Text('Hello World.......!'));
+  }
+}
